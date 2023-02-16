76ec896c259da81dc9c55d338ea7c7596a471d67
SadWIni <skylorun@gmail.com>
2023-02-16 19:30:51 +0300

    Basic functional of Faucet

---
+++ contracts/4_Faucet.sol
@@ -0,0 +1,12 @@
+contract Faucet{
+    // Give otu ether to anything who asks
+    function withdraw(uint withdraw_amount) public{
+        //Limit withdraw amount
+        require(withdraw_amount<=100000000000000000);
+
+        //Send the amount to the address that requested it
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+
+    fallback() external payable{}
+}
