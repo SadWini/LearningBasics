ada22217a6ab06c06143ad80d5f691fe12cf5bf7
SadWini <skylorun@gmail.com>
2023-02-23 18:46:00 +0300

    Rewrote the Faucet contract using the inheritance paradigm

--- contracts/4_Faucet.sol
+++ contracts/4_Faucet.sol
@@ -1,28 +1,37 @@
-contract Faucet{
+pragma solidity >=0.7.0 <0.9.0;
+
+contract owned {
+
+    address owner;
+
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+}
+
+contract mortal is owned{
+    //Contract destructor
+    function destroy() public onlyOwner{
+        selfdestruct(payable(owner));
+    }
+}
+
+contract Faucet is mortal{
     // Give otu ether to anything who asks
     function withdraw(uint withdraw_amount) public{
         
-        address owner;
-        //Initialize Faucet contract: set owner
-        constructor(){
-            owner = msg.sender;
-        } 
-        //Modifier to allow only the owner of the contract to execute a function
-        modifier onlyOwner {
-            require(msg.sender==owner);
-            _;
-        }
-
         //Limit withdraw amount
         require(withdraw_amount<=100000000000000000);
 
         //Send the amount to the address that requested it
         payable(msg.sender).transfer(withdraw_amount);
-
-        //Contract destructor 
-        function destroy() public onlyOwner{
-            selfdestruct(owner);
-        }
     }
 
     fallback() external payable{}
