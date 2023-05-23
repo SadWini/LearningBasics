601a631836bca773ff2efa264d1b369155410930
sadwini <skylorun@gmail.com>
2023-05-23 17:29:54 +0300

    Ded ex.5.8, 5.10

--- Go/GoPL/Chapter5/outline2/outline2.go
+++ Go/GoPL/Chapter5/outline2/outline2.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 )
 
-var depth int
-
 func main() {
 	for _, url := range os.Args[1:] {
 		outline(url)
@@ -27,8 +25,80 @@ func outline(url string) error {
 		return err
 	}
 
+	var depth int
+	startElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			var attribute string
+			for _, attr := range n.Attr {
+				attribute += fmt.Sprintf("%s=%q ", attr.Key, attr.Val)
+			}
+			child := ""
+			if n.Data == "img" && n.FirstChild == nil {
+				child = "/"
+			}
+			if len(attribute) > 1 {
+				fmt.Printf("%*s<%s %s%s>\n", depth*2, "", n.Data, attribute, child)
+			} else {
+				fmt.Printf("%*s<%s%s>\n", depth*2, "", n.Data, child)
+			}
+			depth++
+		} else if n.Type == html.TextNode || n.Type == html.CommentNode {
+			if !(n.Parent.Type == html.ElementNode &&
+				(n.Parent.Data == "script" || n.Parent.Data == "style")) {
+				for _, line := range strings.Split(n.Data, "\n") {
+					line = strings.TrimSpace(line)
+					if line != "" && line != "\n" {
+						fmt.Printf("%*s%s\n", depth*2, "", line)
+					}
+				}
+			}
+		}
+	}
+	endElement := func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			depth--
+			if !(n.Data == "img" && n.FirstChild == nil) {
+				fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+			}
+		}
+	}
 	forEachNode(doc, startElement, endElement)
 	return nil
+}
+
+func ElementById(doc *html.Node, id string) *html.Node {
+	pre := func(doc *html.Node) bool {
+		for _, attr := range doc.Attr {
+			if attr.Key == "id" && attr.Val == id {
+				return true
+			}
+		}
+		return false
+	}
+	if node, found := forEachNodeSearch(doc, pre, nil); found {
+		return node
+	}
+	return nil
+}
+
+func forEachNodeSearch(n *html.Node, pre, post func(n *html.Node) bool) (*html.Node, bool) {
+	if pre != nil {
+		if pre(n) {
+			return n, true
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if node, ok := forEachNodeSearch(c, pre, post); ok {
+			return node, true
+		}
+
+	}
+	if post != nil {
+		if post(n) {
+			return n, true
+		}
+	}
+	return nil, false
 }
 
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
@@ -40,43 +110,5 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	}
 	if post != nil {
 		post(n)
-	}
-}
-
-func startElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		var attribute string
-		for _, attr := range n.Attr {
-			attribute += fmt.Sprintf("%s=%q ", attr.Key, attr.Val)
-		}
-		child := ""
-		if n.Data == "img" && n.FirstChild == nil {
-			child = "/"
-		}
-		if len(attribute) > 1 {
-			fmt.Printf("%*s<%s %s%s>\n", depth*2, "", n.Data, attribute, child)
-		} else {
-			fmt.Printf("%*s<%s%s>\n", depth*2, "", n.Data, child)
-		}
-		depth++
-	} else if n.Type == html.TextNode || n.Type == html.CommentNode {
-		if !(n.Parent.Type == html.ElementNode &&
-			(n.Parent.Data == "script" || n.Parent.Data == "style")) {
-			for _, line := range strings.Split(n.Data, "\n") {
-				line = strings.TrimSpace(line)
-				if line != "" && line != "\n" {
-					fmt.Printf("%*s%s\n", depth*2, "", line)
-				}
-			}
-		}
-	}
-}
-
-func endElement(n *html.Node) {
-	if n.Type == html.ElementNode {
-		depth--
-		if !(n.Data == "img" && n.FirstChild == nil) {
-			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
-		}
 	}
 }
