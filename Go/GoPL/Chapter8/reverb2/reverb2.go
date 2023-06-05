2496584eff47a17d747ee0005e5d31e9d7d3c962
sadwini <skylorun@gmail.com>
2023-06-05 22:33:43 +0300

    Read till 8.8, did ex.8.8

--- Go/GoPL/Chapter8/reverb2/reverb2.go
+++ Go/GoPL/Chapter8/reverb2/reverb2.go
@@ -26,23 +26,41 @@ func main() {
 
 func handleConn(c net.Conn) {
 	input := bufio.NewScanner(c)
-	var wg sync.WaitGroup
+	timeout := time.NewTimer(10 * time.Second)
+	text := make(chan string)
 
-	for input.Scan() {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			echo(c, input.Text(), 1*time.Second)
-		}()
-	}
-	
-	wg.Wait()
-	if con, ok := c.(*net.TCPConn); ok {
-		con.CloseWrite()
-	} else {
-		c.Close()
+	var wg sync.WaitGroup
+	go func() {
+		for input.Scan() {
+			text <- input.Text()
+		}
+		close(text)
+	}()
+
+	for {
+		select {
+		case t, ok := <-text:
+			if ok {
+				wg.Add(1)
+				timeout.Reset(10 * time.Second)
+				go func() {
+					defer wg.Done()
+					echo(c, t, 1*time.Second)
+				}()
+			} else {
+				wg.Wait()
+				c.Close()
+				return
+			}
+		case <-timeout.C:
+			timeout.Stop()
+			c.Close()
+			fmt.Println("disconnect client, no new sounds in the last 10 seconds")
+			return
+		}
 	}
 }
+
 func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
 	time.Sleep(delay)
