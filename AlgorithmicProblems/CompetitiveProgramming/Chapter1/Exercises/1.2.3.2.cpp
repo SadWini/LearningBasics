6bd7545c1f9f9dcdb98f5601732b08dc7b3e93fb
SadWini <skylorun@gmail.com>
2023-06-17 20:38:18 +0300

    Read up to 1.2.4, did 1.2.3(1-3)

---
+++ AlgorithmicProblems/CompetitiveProgramming/Chapter1/Exercises/1.2.3.2.cpp
@@ -0,0 +1,11 @@
+// 1) String str = String.format("%7.3f", 1.4732);
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+    int n = scanf("%d", &n);
+    cout.precision(n+1);
+    cout << M_PI;
+    return 0;
+}
