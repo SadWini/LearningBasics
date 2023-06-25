8500a844723a72933de90cc1eac43b89f1017674
SadWini <skylorun@gmail.com>
2023-06-25 23:00:31 +0300

    Finished to do super easy tasks

--- AlgorithmicProblems/CompetitiveProgramming/Chapter1/Exercises/1.3/UVa12250.cpp
+++ AlgorithmicProblems/CompetitiveProgramming/Chapter1/Exercises/1.3/UVa12250.cpp
@@ -1,3 +1,16 @@
-//
-// Created by skylorun on 24.06.23.
-//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k = 1;
+    string str;
+    while(cin >> str && str != "#") {
+        if (str == "HELLO") puts("ENGLISH");
+        else if (str == "HOLA") puts("SPANISH");
+        else if (str == "HALLO")  puts("GERMAN");
+        else if (str == "BONJOUR")  puts("FRENCH");
+        else if (str == "CIAO") puts("ITALIAN");
+        else if (str == "ZDRAVSTVUJTE") puts("RUSSIAN");
+        else puts("UNKNOWN");
+    }
+}
