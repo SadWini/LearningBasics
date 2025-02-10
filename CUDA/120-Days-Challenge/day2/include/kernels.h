fa6732e241d1a418205556f88d4629d1a9d726e7
SadWini <103163495+SadWini@users.noreply.github.com>
2025-02-10 23:40:58 +0300

    Read day 2, checked links, read chapter 3 from programming guide

---
+++ CUDA/120-Days-Challenge/day2/include/kernels.h
@@ -0,0 +1,6 @@
+#ifndef __KERNELS_H__
+#define __KERNELS_H__
+
+__global__ void dummyKernel();
+
+#endif
---
+++ CUDA/120-Days-Challenge/day2/src/main.cu
@@ -0,0 +1,7 @@
+#include <cuda_runtime.h>
+#include "kernels.h"
+int main() {
+    dummyKernel<<<2, 4>>>();
+    cudaDeviceSynchronize();
+    return 0;
+}
