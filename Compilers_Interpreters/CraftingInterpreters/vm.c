a670870cef6c6c17f924cd992aab956a5735f61b
SadWini <103163495+SadWini@users.noreply.github.com>
2025-02-12 23:26:33 +0300

    Read till 3-color invariants and started processions objects from roots

--- Compilers_Interpreters/CraftingInterpreters/vm.c
+++ Compilers_Interpreters/CraftingInterpreters/vm.c
@@ -60,6 +60,10 @@ void initVM() {
     initTable(&vm.strings);
     initTable(&vm.globals);
 
+    vm.grayCount = 0;
+    vm.grayCapacity = 0;
+    vm.grayStack = NULL;
+
     defineNative("clock", clockNative);
 }
 
