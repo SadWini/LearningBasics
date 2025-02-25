2434c75f8b3d20b0e6161ce3424fbd2493883e36
SadWini <103163495+SadWini@users.noreply.github.com>
2025-02-25 23:59:39 +0300

    Read untill garbage collection bugs

--- Compilers_Interpreters/CraftingInterpreters/memory.c
+++ Compilers_Interpreters/CraftingInterpreters/memory.c
@@ -12,11 +12,19 @@
 #include  "debug.h"
 #endif
 
+#define GC_HEAP_GROW_FACTOR 2
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    vm.bytesAllocated += newSize - oldSize;
+
     if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
         collectGarbage();
 #endif
+    }
+
+    if (vm.bytesAllocated < vm.nextGC) {
+        collectGarbage();
     }
 
     if (newSize == 0) {
@@ -180,6 +188,7 @@ static void sweep() {
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
+    size_t before = vm.bytesAllocated;
 #endif
 
     markRoots();
@@ -187,8 +196,12 @@ void collectGarbage() {
     tableRemoveWhite(&vm.strings);
     sweep();
 
+    vm.nextGC = vm.bytesAllocated * GC_HEAP_GROW_FACTOR;
+
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
+    printf("    collected %llu bytes (from %llu to %llu) next at %llu\n",
+        before - vm.bytesAllocated, before, vm.bytesAllocated, vm.nextGC);
 #endif
 }
 
--- Compilers_Interpreters/CraftingInterpreters/vm.c
+++ Compilers_Interpreters/CraftingInterpreters/vm.c
@@ -56,9 +56,13 @@ static void defineNative(const char* name, NativeFn function) {
 
 void initVM() {
     resetStack();
-    vm.objects = NULL;
+
     initTable(&vm.strings);
     initTable(&vm.globals);
+
+    vm.objects = NULL;
+    vm.bytesAllocated = 0;
+    vm.nextGC = 1024 * 1024;
 
     vm.grayCount = 0;
     vm.grayCapacity = 0;
