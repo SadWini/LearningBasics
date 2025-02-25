0c9d10cbfddc67a71f61caf1119e742b98f834a2
SadWini <103163495+SadWini@users.noreply.github.com>
2025-02-26 00:16:56 +0300

    Finished chapter about garbage collection

--- Compilers_Interpreters/CraftingInterpreters/vm.c
+++ Compilers_Interpreters/CraftingInterpreters/vm.c
@@ -176,8 +176,8 @@ static bool isFalsey(Value value) {
 }
 
 static void concatenate() {
-    ObjString* b = AS_STRING(pop());
-    ObjString* a = AS_STRING(pop());
+    ObjString* b = AS_STRING(peek(0));
+    ObjString* a = AS_STRING(peek(1));
 
     int length = a->length + b->length;
     char* chars = ALLOCATE(char, length + 1);
@@ -185,6 +185,8 @@ static void concatenate() {
     memcpy(chars + a->length, b->chars, b->length);
     chars[length] = '\0';
     ObjString* result = takeString(chars, length);
+    pop();
+    pop();
     push(OBJ_VAL(result));
 }
 
